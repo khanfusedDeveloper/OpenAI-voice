92cc59f18a25aa9ae713ab88edbaa44a86bea5cd
mkhan399 <mkhan399@gatech.edu>
2023-05-04 00:35:12 -0400

    started implementing bootstrap into the project.

--- openAI API/app.js
+++ openAI API/app.js
@@ -3,6 +3,11 @@ const app = express(); //to use express js
 const bodyParser = require("body-parser"); //parsing post from html
 const https = require("https");
 const path = require("path");
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -11,7 +16,7 @@ app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
 
-    res.render('index', {pageTitle: "Welcome to my page"});
+    res.render('index', {pageTitle: "Welcome to my page.TEST"});
 
 });
 
