93cb109afc2a93d286a1339cbd9537b8980877de
mkhan399 <mkhan399@gatech.edu>
2023-08-10 19:05:07 -0400

    Adding the backend node js server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express(); //to use express js
+const bodyParser = require("body-parser"); //parsing post from html
+const https = require("https");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res){    
+
+});
+
+app.listen(3000, function(){
+
+
+    console.log("server is up and running");
+})
